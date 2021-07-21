9a1ad17f263b75ad2ae2525bcfd1b0acb7331a06
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2021-07-21 18:28:50 -0500

    loader added

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const quoteText = document.getElementById("quote");
 const quoteAuthor = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let quotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function completed() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 function newQuote(){
+    loading();
     const numberRandom = Math.floor(Math.random() * quotes.length)
     const quote = quotes[numberRandom];
     
@@ -22,9 +34,11 @@ function newQuote(){
         quoteText.classList.remove("long-quote");
     }
     quoteText.textContent = quote.text;
+    completed();
 }
 
 async function getQuotes() {
+    loading();
     const api_url = "https://type.fit/api/quotes";
     try {
         const response = await fetch(api_url);
@@ -40,9 +54,7 @@ function tweetQuote() {
     window.open(twitter_url, "_blank");
 }
 
-
 twitterBtn.addEventListener("click", tweetQuote);
 newQuoteBtn.addEventListener("click", newQuote);
-
 
 getQuotes();
